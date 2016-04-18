e4d8e22a61ac975618800804c78a9e85fa3fd394
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-04-18 16:38:38 -0700

    webhook route

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.post('/webhook', function (req,res){
+  console.log('Webhook received!')
+  res.status(200);
+  res.send('Message Received!')
+});
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port ', process.env.PORT || 8080);
