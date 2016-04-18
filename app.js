71f6be600c67b69802eb845087ccaef11328763c
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-04-18 16:50:14 -0700

    get request webhook

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.post('/webhook', function (req,res){
-  console.log('Webhook received!')
-  res.status(200);
-  res.send('Message Received!')
-});
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'verify_token') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port ', process.env.PORT || 8080);
