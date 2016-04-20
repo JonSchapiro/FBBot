4d1dba228824088efb4b170cb589238cfa022e87
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-04-20 14:27:30 -0700

    better and more responses added to brain

--- brain.js
+++ brain.js
@@ -15,34 +15,64 @@ var Brain = function(){
 };
 
 Brain.prototype.getDictionary = function(){
-  var categories=["greeting","loyalty","nutrition","info","locations","realEstate","tellus","opening"]
+  var categories=["greeting","loyalty","nutrition","info","locations","realEstate","tellus","opening","employment"]
   
   return {
     "hi":categories[categories.indexOf("greeting")],
+    "hello":categories[categories.indexOf("greeting")],
     "hey":categories[categories.indexOf("greeting")],
     "yo":categories[categories.indexOf("greeting")],
+    "loyalty":categories[categories.indexOf("loyalty")],
+    "loyal":categories[categories.indexOf("loyalty")],
     "card":categories[categories.indexOf("loyalty")],
     "meltcard":categories[categories.indexOf("loyalty")],
     "nutrition":categories[categories.indexOf("nutrition")],
     "information":categories[categories.indexOf("info")],
-    "info":categories[categories.indexOf("info")]
+    "info":categories[categories.indexOf("info")],
+    "locatio":categories[categories.indexOf('locations')],
+    "locat":categories[categories.indexOf('locations')],
+    "locate":categories[categories.indexOf('locations')],
+    "location":categories[categories.indexOf('locations')],
+    "locations":categories[categories.indexOf('locations')],
+    "located":categories[categories.indexOf('locations')],
+    "where":categories[categories.indexOf('locations')],
+    "estate":categories[categories.indexOf('realEstate')],
+    "franchising":categories[categories.indexOf('realEstate')],
+    "franchise":categories[categories.indexOf('realEstate')],
+    "franchised":categories[categories.indexOf('realEstate')],
+    "property":categories[categories.indexOf('realEstate')],
+    "bad":categories[categories.indexOf('tellus')],
+    "rude":categories[categories.indexOf('tellus')],
+    "worst":categories[categories.indexOf('tellus')],
+    "dirty":categories[categories.indexOf('tellus')],
+    "terrible":categories[categories.indexOf('tellus')],
+    "sucks":categories[categories.indexOf('tellus')],
+    "opening":categories[categories.indexOf('opening')],
+    "job":categories[categories.indexOf('employment')],
+    "opportunity":categories[categories.indexOf('employment')]
   }
-}
+};
 
 Brain.prototype.getMap = function() {
   return {
     "greeting":this.greet,
     "loyalty":this.loyalty,
     "nutrition":this.nutrition,
-    "info":this.information
+    "info":this.information,
+    "locations":this.locations,
+    "realEstate":this.realEstate,
+    "tellus":this.tellus,
+    "opening":this.opening,
+    "employment":this.employment,
+    "default":this.default
   }
-}
+};
 
 Brain.prototype.match = function(message,sendMessageCallback){
   var messagesToSend = [];
 
   if (message.length > 0){
-    var words = message.split(' ');
+    var words = message.toLowerCase().split(' ');
 
     for (var i = 0; i < words.length; i++){
       var action = this.dictionary[words[i]];
@@ -65,19 +95,49 @@ Brain.prototype.greet = function(){
 };
 
 Brain.prototype.loyalty = function(){
-  var msg = "You should definitely get a melt card. You'll get some free stuff!";
+  var msg = "Hey there! Thanks for expressing interest in a Melt Card. To sign up, please visit: https://themelt.com/meltcard";
   return msg;
-}
+};
 
 Brain.prototype.nutrition = function(){
-  var msg = "Nutrition is what keeps us alive!";
+  var msg = "Thank you for your interest in The Melt! Check out our nutrition facts at https://themelt.com/nutrition";
   return msg;
-}
+};
 
 Brain.prototype.information = function(){
-  var msg = "If you want more information, make sure to visit our FAQ section."
+  var msg = " Hi there! If you want more information about The Melt , make sure to visit our FAQ section. https://themelt.com/faq";
   return msg;
-}
+};
+
+Brain.prototype.locations = function(){
+  var msg = "Hi there, we have locations in Northern and Southern California + Colorado. Check out the best location near you: https://themelt.com/locations/all";
+  return msg;
+};
+
+Brain.prototype.realEstate = function(){
+  var msg = "Thank you for your interest in The Melt. At this time, all stores are company owned. Feel free to reach out with any additional questions you may have. Cheers.";
+  return msg;
+};
+
+Brain.prototype.tellus = function(){
+  var msg = "I’m so sorry for your less than happy experience at The Melt. I will immediately notify our Operations team so we can be better. Can you kindly share your mailing address? I’d love to make it up to you by sending a few gift cards your way.";
+  return msg;
+};
+
+Brain.prototype.opening = function(){
+  var msg = "Hi there! For more information aobut store openings, check out: https://themelt.com/locations/all";
+  return msg;
+};
+
+Brain.prototype.employment = function(){
+  var msg = "Thanks for inquiring about employment at The Melt! Please refer to the job section of our website for more information. https://themelt.com/jobs";
+  return msg;
+};
+
+Brain.prototype.default = function(){
+  var msg = "Thanks for reaching out to us! \nEnter one of the following words to learn more: \n1) General Information\n2) Melt Card\n3) Locations\n4) Nutrition \n5) Store Openings \n6) Franchise Opportunities \n7) Catering \n8) Negative Experience";
+  return msg;
+};
 
 Brain.prototype.constructMessage = function(messages){
   var finalMessage = [];
@@ -98,7 +158,7 @@ Brain.prototype.determineRelevance = function(messages){
   }
   
   var relevanceStrength = 0; //message occurence
-  var relevanceFunc; //message function
+  var relevanceFunc = null; //message function
   for (func in messagesCount){
     //prioritize greeting
     if (func == "greeting"){
@@ -111,7 +171,12 @@ Brain.prototype.determineRelevance = function(messages){
       }
     }
   }
+ if (relevanceFunc == null){
+  relevantMessages.push(this.map["default"])
+ } else {
   relevantMessages.push(relevanceFunc);
+ }
+  
   return relevantMessages;
 };
 
